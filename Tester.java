c8024e28447a83f2c3ad57ad5e2286e4144580d7
Yin-Y-M <117802747+Yin-Y-M@users.noreply.github.com>
2023-02-16 09:55:36 -0800

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,67 @@
+//Imports
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//IsAnA is a StringChecker class used for testing
+class IsAnA implements StringChecker {
+  @Override
+  public boolean checkString(String s) {
+      return !s.equals("a");
+  }
+}
+
+public class Tester {
+  //Tests filter on arbitrary sequence of strings
+  @Test 
+  public void testFilterArbitrary() {
+      List<String> myList = Arrays.asList("a", "hello","b", "a","c");
+      List<String> expected = Arrays.asList("hello","b","c");
+
+      assertEquals(expected,ListExamples.filter(myList, new IsAnA()));
+  }
+
+  //Tests filter on an empty list
+  @Test 
+  public void testFilterEmpty() {
+      List<String> myList = Arrays.asList();
+      List<String> expected = Arrays.asList();
+
+      assertEquals(expected,ListExamples.filter(myList, new IsAnA()));
+  }
+
+  //Tests merge on arbitrary sequence of letters
+  @Test(timeout = 500)
+  public void testMergeArbitrary() {
+      List<String> list1 = Arrays.asList("a","c", "e");
+      List<String> list2 = Arrays.asList("a","b", "f","g");
+
+      List<String> merged = ListExamples.merge(list1, list2);
+      List<String> expected = Arrays.asList("a", "a","b","c","e","f","g");
+
+      assertEquals(expected, merged);
+  }
+
+  //Tests merge on empty lists
+  @Test 
+  public void testMergeEmpty() {
+    List<String> list1 = Arrays.asList();
+    List<String> list2 = new ArrayList<>();
+
+    List<String> merged = ListExamples.merge(list1, list2);
+    List<String> expected=Arrays.asList();
+
+    assertEquals(expected, merged);
+
+    list1 = Arrays.asList("asdf");
+    list2 = new ArrayList<>();
+
+    merged = ListExamples.merge(list1, list2);
+    expected=Arrays.asList("asdf");
+
+    assertEquals(expected, merged);
+  }
+}
---
+++ grade.sh
@@ -0,0 +1,29 @@
+CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+NUMTEST=4
+rm -rf student-submission
+git clone $1 student-submission
+
+cp Tester.java student-submission
+cp -r lib student-submission
+cd student-submission
+
+if [[ -f ListExamples.java ]]
+then
+    echo "file found"
+    javac ListExamples.java
+    if [[ $? -ne 0 ]]
+    then 
+        echo "error in compiling"
+    else
+        javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
+        java -cp ".;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore Tester > test.txt
+        echo $(($NUMTEST-`head -n 2 test.txt | grep -o "E" | wc -l`)) out of $NUMTEST points achieved!
+
+    fi
+
+else
+    echo "file not found"
+    exit 1
+fi
+
+echo 'Finished cloning'
